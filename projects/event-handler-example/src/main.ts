d0f2a8348f81c52d2974f36c85b17059285ede53
Redditbara <copybara@reddit.com>
2023-06-20 15:20:16 -0400

    Project import generated by Copybara.
    GitOrigin-RevId: 7356e7f6ac41ef495c79edde5644d45b34fe0417

--- projects/event-handler-example/src/main.ts
+++ projects/event-handler-example/src/main.ts
@@ -1,76 +1,79 @@
-import {
-  PostSubmit,
-  CommentUpdate,
-  CommentReport,
-  SubredditSubscribe,
-  CommentSubmit,
-  Metadata,
-  AppInstall,
-  AppUpgrade,
-} from '@devvit/protos';
-import { Devvit, getFromMetadata, Header } from '@devvit/public-api';
+import { Devvit } from '@devvit/public-api-next';
 
 Devvit.addTrigger({
-  event: Devvit.Trigger.PostSubmit,
-  handler(request: PostSubmit) {
-    console.log(`Received OnPostSubmit event:\n${JSON.stringify(request)}`);
+  event: 'PostSubmit',
+  onEvent(event, _context) {
+    console.log(`Received OnPostSubmit event`);
   },
 });
 
 Devvit.addTrigger({
-  events: [Devvit.Trigger.PostUpdate, Devvit.Trigger.PostReport],
-  handler(request) {
-    if (request.type == Devvit.Trigger.PostUpdate) {
-      console.log(`Received OnPostUpdate event:\n${JSON.stringify(request)}`);
-    } else if (request.type === Devvit.Trigger.PostReport) {
-      console.log(`Received OnPostReport event:\n${JSON.stringify(request)}`);
+  events: ['PostUpdate', 'PostReport'],
+  onEvent(event, _context) {
+    if (event.type == 'PostUpdate') {
+      console.log('Received OnPostUpdate event');
+    } else if (event.type === 'PostReport') {
+      console.log('Received OnPostReport event');
     }
   },
 });
 
 Devvit.addTrigger({
-  event: Devvit.Trigger.CommentSubmit,
-  handler(request: CommentSubmit, metadata?: Metadata) {
-    console.log(`Received OnCommentSubmit event:\n${JSON.stringify(request)}`);
+  event: 'PostDelete',
+  onEvent(event, _context) {
+    console.log('Received OnPostDelete event');
+  },
+});
 
-    if (request.author?.id === getFromMetadata(Header.AppUser, metadata)) {
+Devvit.addTrigger({
+  event: 'CommentSubmit',
+  onEvent(event, context) {
+    console.log('Received OnCommentSubmit event');
+    if (event.author?.id === context.appAccountId) {
       console.log('hey! I created this comment; not going to respond');
     }
   },
 });
 
 Devvit.addTrigger({
-  event: Devvit.Trigger.CommentUpdate,
-  async handler(request: CommentUpdate) {
-    console.log(`Received OnCommentUpdate event:\n${JSON.stringify(request)}`);
+  event: 'CommentUpdate',
+  onEvent(event, _context) {
+    console.log('Received OnCommentUpdate event');
   },
 });
 
 Devvit.addTrigger({
-  event: Devvit.Trigger.CommentReport,
-  async handler(request: CommentReport) {
-    console.log(`Received OnCommentReport event:\n${JSON.stringify(request)}`);
+  event: 'CommentReport',
+  onEvent(event, _context) {
+    console.log('Received OnCommentReport event');
   },
 });
 
 Devvit.addTrigger({
-  event: Devvit.Trigger.SubredditSubscribe,
-  async handler(request: SubredditSubscribe) {
-    console.log(`Received OnSubredditSubscribe event:\n${JSON.stringify(request)}`);
+  event: 'CommentDelete',
+  onEvent(event, _context) {
+    console.log('Received OnCommentDelete event');
   },
 });
 
 Devvit.addTrigger({
-  event: Devvit.Trigger.AppInstall,
-  async handler(request: AppInstall) {
-    console.log(`Received AppInstall event:\n${JSON.stringify(request)}`);
+  event: 'SubredditSubscribe',
+  onEvent(event, _context) {
+    console.log('Received OnSubredditSubscribe event');
   },
 });
 
 Devvit.addTrigger({
-  event: Devvit.Trigger.AppUpgrade,
-  async handler(request: AppUpgrade) {
-    console.log(`Received AppUpgrade event:\n${JSON.stringify(request)}`);
+  event: 'AppInstall',
+  onEvent(event, _context) {
+    console.log('Received OnAppInstall event');
+  },
+});
+
+Devvit.addTrigger({
+  event: 'AppUpgrade',
+  onEvent(event, _context) {
+    console.log('Received OnAppUpgrade event');
   },
 });
 
