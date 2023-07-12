af79b9329c8073c4ee4e723bf0791ae007d9523e
Redditbara <copybara@reddit.com>
2023-07-12 09:22:08 -0400

    Project import generated by Copybara.
    GitOrigin-RevId: 7e7052ffce6c3e4926bc96629d0ca7a65ce5b8d6

--- projects/send-me-a-message-example/src/main.ts
+++ projects/send-me-a-message-example/src/main.ts
@@ -1,66 +1,28 @@
-import { Context, Devvit, getFromMetadata, Header, NameLookup } from '@devvit/public-api-old';
+import { Devvit } from '@devvit/public-api';
 import { StringUtil } from '@devvit/shared-types/StringUtil.js';
 
-const PrivateMessages = Devvit.use(Devvit.Types.RedditAPI.PrivateMessages);
-const nameLookup = new NameLookup();
+Devvit.addMenuItem({
+  label: 'Send PM',
+  location: 'subreddit',
+  onPress: async (_event, context) => {
+    const { userId, reddit, ui } = context;
 
-export default Devvit.addAction({
-  name: 'Send PM',
-  description: 'Send yourself a private message',
-  context: Context.SUBREDDIT,
-  handler: async (_event, metadata) => {
-    const userId = getFromMetadata(Header.User, metadata);
-    const subredditId = getFromMetadata(Header.Subreddit, metadata);
+    if (!userId) {
+      ui.showToast('Unknown user!');
+      return;
+    }
+
     try {
-      if (!userId) {
-        return {
-          success: false,
-          message: "I don't know who you are!",
-        };
-      }
-
-      if (!subredditId) {
-        return {
-          success: false,
-          message: "I don't know what subreddit you're currently in...",
-        };
-      }
-
-      // The one-at-a-time way:
-      const to = await nameLookup.get(userId);
-      const fromSr = await nameLookup.get(subredditId);
-      // Or the many-at-a-time way:
-      const idMap = await nameLookup.getAll([userId, subredditId]);
-      // Verify both gave back the same answer!
-      console.log(JSON.stringify({ to, fromSr, idMap }));
-      if (idMap[userId] !== to) {
-        return {
-          success: false,
-          message: `Assertion failed: idMap[userId] !== to (${idMap[userId]} !== ${to})`,
-        };
-      }
-      if (idMap[subredditId] !== fromSr) {
-        return {
-          success: false,
-          message: `Assertion failed: idMap[subredditId] !== fromSr (${idMap[subredditId]} !== ${fromSr})`,
-        };
-      }
-
-      await PrivateMessages.Compose({
-        to,
-        fromSr,
+      await reddit.sendPrivateMessage({
+        to: userId,
         subject: 'Hello World!',
         text: 'Hey, the test action worked!',
       });
-      return {
-        success: true,
-        message: 'Message sent!',
-      };
+      ui.showToast('Message sent!');
     } catch (err) {
-      return {
-        success: false,
-        message: StringUtil.caughtToString(err),
-      };
+      ui.showToast(StringUtil.caughtToString(err));
     }
   },
 });
+
+export default Devvit;
--- projects/test-pm/src/main.ts
+++ projects/test-pm/src/main.ts
@@ -1,32 +1,28 @@
-import { Context, Devvit, RedditAPIClient, UserContext } from '@devvit/public-api-old';
-const reddit = new RedditAPIClient();
+import { Devvit } from '@devvit/public-api';
 
-Devvit.addAction({
-  name: 'read pms',
-  description: 'Reads your private messages',
-  context: Context.POST,
-  userContext: UserContext.MEMBER,
-  handler: async (_ctx, md) => {
-    const pms = await reddit.getMessages({ type: 'sent', limit: 10 }, md);
+Devvit.addMenuItem({
+  label: 'read pms',
+  location: 'post',
+  onPress: async (_event, context) => {
+    const { reddit, ui } = context;
+    const pms = await reddit.getMessages({ type: 'sent', limit: 10 });
     let i = 0;
     for await (const pm of pms) {
       console.log(++i);
       console.log({ pm });
     }
-    return { success: true, message: 'nice' };
+    ui.showToast('nice');
   },
 });
 
-Devvit.addAction({
-  name: 'mark all as read',
-  description: 'Marks all your messages as read',
-  context: Context.POST,
-  userContext: UserContext.MEMBER,
-  handler: async (_ctx, md) => {
-    await reddit.markAllMessagesAsRead(md);
-    return { success: true, message: 'marked all as read' };
+Devvit.addMenuItem({
+  label: 'mark all as read',
+  location: 'post',
+  onPress: async (_event, context) => {
+    const { reddit, ui } = context;
+    await reddit.markAllMessagesAsRead();
+    ui.showToast('marked all as read');
   },
 });
 
-// Visit developers.reddit.com/docs to view documentation for the Devvit api
 export default Devvit;
