3656d087e7f43771499a20f4f67c045952ff4b85
Redditbara <copybara@reddit.com>
2023-08-08 14:35:16 -0700

    Project import generated by Copybara.
    GitOrigin-RevId: 6bfcba58492a11fed6a1cbfe83dfabf24b9b5a67

--- project-templates/hello-mod-mail/src/main.ts
+++ project-templates/hello-mod-mail/src/main.ts
@@ -1,10 +1,10 @@
 import { Devvit } from '@devvit/public-api';
 
-Devvit.configure({redditAPI: true})
+Devvit.configure({ redditAPI: true });
 
 Devvit.addTrigger({
   event: 'ModMail',
-  onEvent: async (event, context)  => {
+  onEvent: async (event, context) => {
     // Apps receive this event when:
     // 1. A new modmail conversation thread is created
     // 2. A new modmail message is added to an existing conversation
@@ -13,14 +13,20 @@ Devvit.addTrigger({
     // Example conversation ID: ModmailConversation_1mzkfh
     // We are fetching the latest conversation state from Reddit API
     const conversationId = event.conversationId;
-    const result = await context.reddit.modMail.getConversation({ conversationId: conversationId, markRead: false });
-    console.log(`Received conversation with subject: ${result.conversation?.subject}`);
+    const result = await context.reddit.modMail.getConversation({
+      conversationId: conversationId,
+      markRead: false,
+    });
 
-    // Looking up the incoming message from trigger event
-    // Example Message ID: ModmailMessage_2ch154
-    const messageId = event.messageId.split("_")[1];
-    const message =  result.messages[messageId]
-    console.log(`Received modmail message: ${JSON.stringify(message)}`);
+    if (result.conversation) {
+      console.log(`Received conversation with subject: ${result.conversation.subject}`);
+
+      // Looking up the incoming message from trigger event
+      // Example Message ID: ModmailMessage_2ch154
+      const messageId = event.messageId.split('_')[1];
+      const message = result.conversation.messages[messageId];
+      console.log(`Received modmail message: ${JSON.stringify(message)}`);
+    }
   },
 });
 
