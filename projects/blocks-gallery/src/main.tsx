3c3819189a3505b69c6bf85d05e0b128d4369297
Redditbara <copybara@reddit.com>
2023-08-24 13:16:17 -0400

    Project import generated by Copybara.
    GitOrigin-RevId: df712d8dea0c85710729dc9d63239c651901da70

--- projects/blocks-gallery/src/main.tsx
+++ projects/blocks-gallery/src/main.tsx
@@ -1,7 +1,25 @@
 import { ContextAPIClients, Devvit } from '@devvit/public-api';
-
 import { BlockGallery } from './components/BlockGallery.js';
 import { GalleryState } from './state/state.js';
+
+Devvit.configure({
+  redditAPI: true,
+});
+
+Devvit.addMenuItem({
+  label: 'Blocks Gallery: Create a post',
+  location: 'subreddit',
+  async onPress(_, { reddit, ui }) {
+    const currentSubreddit = await reddit.getCurrentSubreddit();
+    await reddit.submitPost({
+      title: 'Blocks Gallery',
+      subredditName: currentSubreddit.name,
+      preview: <text>Loading blocks gallery</text>,
+    });
+
+    ui.showToast('Posted a blocks gallery!');
+  },
+});
 
 Devvit.addCustomPostType({
   name: 'Blocks Gallery',
