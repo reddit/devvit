80972e71fb2af07dccee18b7e064ef416387a518
Redditbara <copybara@reddit.com>
2023-10-02 11:55:12 -0700

    Project import generated by Copybara.
    GitOrigin-RevId: 7736f8ae8d34aff541e7b47695630c46acae680a

--- projects/blocks-gallery/src/components/Columns.tsx
+++ projects/blocks-gallery/src/components/Columns.tsx
@@ -11,9 +11,11 @@ export const Columns = ({ count, children }: ColumnsProps): JSX.Element => {
   // Divide the children into rows with N (count) children each.
   // Pad the last row if incomplete.
   for (let i = 0; i < children.length; i += count) {
-    const row = children.slice(i, i + count);
+    const row = children
+      .slice(i, i + count)
+      .map((child) => <vstack width={100.0 / count}>{child}</vstack>);
     const rowHasRoom = (): boolean => row.length < count;
-    while (rowHasRoom()) row.push(<hstack grow />);
+    while (rowHasRoom()) row.push(<hstack width={100.0 / count} />);
     rows.push(<hstack gap="small">{row}</hstack>);
   }
 
