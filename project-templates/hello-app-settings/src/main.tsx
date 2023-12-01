1975d81d5fe71a6735423a65fc69e5458de61cb1
Redditbara <copybara@reddit.com>
2023-11-30 16:51:58 -0800

    Project import generated by Copybara.
    GitOrigin-RevId: e4eeee93088f95eb344aee7abb54a0848531ea9e

---
+++ project-templates/hello-app-settings/src/main.tsx
@@ -0,0 +1,43 @@
+import { Devvit } from '@devvit/public-api';
+
+Devvit.configure({
+  http: true,
+});
+
+Devvit.addAppSettings([
+  {
+    // This is the name of the setting which can be used to retrieve the value of the setting
+    name: 'weather-api-key',
+    // This label is used to provide more information in the CLI or in dev portal
+    label: 'My weather api key',
+    placeholder: 'Add your weather api key here',
+    type: 'string',
+    isSecret: true,
+  },
+]);
+
+Devvit.addCustomPostType({
+  name: 'Hello Blocks',
+  render: (context) => {
+    const fetchWeatherInfo = async () => {
+      const apiKey: string | undefined = await context.settings.get('weather-api-key');
+      if (apiKey) {
+        const request = new Request(`https://weather.com`, {
+          headers: { Authorization: apiKey },
+        });
+        const response = await fetch(request);
+        console.log('response ', response);
+      }
+    };
+    // Your custom post layout goes here!
+    return (
+      <vstack padding="medium" cornerRadius="medium" gap="medium" alignment="middle">
+        <button appearance="primary" onPress={() => fetchWeatherInfo()}>
+          Click me!
+        </button>
+      </vstack>
+    );
+  },
+});
+
+export default Devvit;
