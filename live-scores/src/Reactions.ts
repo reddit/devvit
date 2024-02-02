5b8d8b026c931a2ce904bef6a4ab85d0eed5df00
Redditbara <copybara@reddit.com>
2024-02-02 13:31:34 -0700

    Project import generated by Copybara.
    GitOrigin-RevId: df6be32f1d782958cf8b723aad4bd3d34691d584

--- live-scores/src/Reactions.ts
+++ live-scores/src/Reactions.ts
@@ -65,7 +65,8 @@ export async function getAllReactionScores(
   const scores = await redis.hgetall(reactionsHashKey(postId, eventId));
   // console.log('scores', scores);
   return reactions.map((reaction) => {
-    const score = scores ? parseInt(scores[reaction.id]) : 0;
+    const scoreValue = scores ? scores[reaction.id] : '0';
+    const score = scoreValue ? parseInt(scoreValue) : 0;
     return { reaction, count: score };
   });
 }
