dcef6eb29e83a726d0bb4d63b82a1e50f69b7c4f
Redditbara <copybara@reddit.com>
2024-05-23 16:10:39 -0700

    Project import generated by Copybara.
    GitOrigin-RevId: 5d7963ecc69a9c7d65561ae1f7b53edd1b47c1da

---
+++ devvit-docs/src/components/IconTileGrid/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+export default function IconTileGrid({ children }): JSX.Element {
+  return <div className={styles.container}>{children}</div>;
+}
