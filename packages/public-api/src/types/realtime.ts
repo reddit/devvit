1d2164703daabe7da4a7917df0a25c37834f3fe4
Redditbara <copybara@reddit.com>
2024-05-23 14:01:16 -0700

    Project import generated by Copybara.
    GitOrigin-RevId: e26c8b72ecc66226c3fddeddd925b3b98133fb8a

--- packages/public-api/src/types/realtime.ts
+++ packages/public-api/src/types/realtime.ts
@@ -12,7 +12,7 @@ export type ChannelOptions = {
   /** Name of the channel */
   name: string;
   /** Called every time a message is received on this channel */
-  onMessage: (data: Data) => void | Promise<void>;
+  onMessage: (data: Data) => void;
   /** Optional hook to be informed when the channel has connected */
   onSubscribed?: (() => void | Promise<void>) | undefined;
   /** Optional hook to be informed when the channel has disconnected */
