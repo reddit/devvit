6d1488a09d8573ca023be037080731c5a9329560
Redditbara <copybara@reddit.com>
2024-05-30 13:15:40 +0100

    Project import generated by Copybara.
    GitOrigin-RevId: 0bf1a62c1c96c706cc66947a6e7941f76debd71e

---
+++ devvit-docs/src/components/TemplateTileGrid/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+export default function TemplateTileGrid({ children }): JSX.Element {
+  return <div className={styles.grid}>{children}</div>;
+}
