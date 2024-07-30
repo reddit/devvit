f701fbe4800a04821d905b00901a55f767dd519b
Redditbara <copybara@reddit.com>
2024-07-30 19:00:13 -0400

    Project import generated by Copybara.
    GitOrigin-RevId: 61a1db8201a8f4b0b863cd485f1a1792c4010cce

--- packages/cli/bin/devvit.js
+++ packages/cli/bin/devvit.js
@@ -1,4 +1,6 @@
-#!/usr/bin/env node
+#!/usr/bin/env -S node --no-warnings=ExperimentalWarning
+// JSON imports are experimental, so we need to disable the warning
+
 import { Errors, flush, run } from '@oclif/core';
 
 run(undefined, import.meta.url)
