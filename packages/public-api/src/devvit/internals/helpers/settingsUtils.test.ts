39958efabfbd3e2de54e366e93aecbc4f43d44da
Redditbara <copybara@reddit.com>
2024-07-30 11:39:15 +0200

    Project import generated by Copybara.
    GitOrigin-RevId: 8f65a4fa6525fb8ac3429f821566bbdbeb6eaa0f

---
+++ packages/public-api/src/devvit/internals/helpers/settingsUtils.test.ts
@@ -0,0 +1,67 @@
+import { extractSettingsFields } from './settingsUtils.js';
+import type { SettingsFormFieldGroup, ValidatedStringField } from '../../../types/index.js';
+
+describe('settings utils', () => {
+  const stringField: ValidatedStringField = {
+    type: 'string',
+    name: 'top_level',
+    label: 'top level',
+    onValidate: () => {
+      return 'invalid';
+    },
+  };
+
+  describe('with no field groups', () => {
+    it('an empty array if emtpy array is passed', () => {
+      expect(extractSettingsFields([])).toEqual([]);
+    });
+
+    it('returns plain fields as is', () => {
+      expect(extractSettingsFields([stringField])).toEqual([stringField]);
+    });
+  });
+
+  describe('with field groups', () => {
+    const nestedStringField: ValidatedStringField = {
+      type: 'string',
+      name: '1st_group_string',
+      label: '1st group string',
+      onValidate: () => {
+        return 'invalid';
+      },
+    };
+    const groupFieldOneDeep: SettingsFormFieldGroup = {
+      type: 'group',
+      label: '1st group',
+      fields: [nestedStringField],
+    };
+    const groupFieldTwoDeep: SettingsFormFieldGroup = {
+      type: 'group',
+      label: '2nd group',
+      fields: [groupFieldOneDeep],
+    };
+    const groupFieldThreeDeep: SettingsFormFieldGroup = {
+      type: 'group',
+      label: '3rd group',
+      fields: [groupFieldOneDeep],
+    };
+
+    it('unwraps nestedness one level deep', () => {
+      expect(extractSettingsFields([groupFieldOneDeep])).toEqual([nestedStringField]);
+    });
+
+    it('unwraps nestedness two levels deep', () => {
+      expect(extractSettingsFields([groupFieldTwoDeep])).toEqual([nestedStringField]);
+    });
+
+    it('unwraps nestedness three levels deep', () => {
+      expect(extractSettingsFields([groupFieldThreeDeep])).toEqual([nestedStringField]);
+    });
+
+    it('unwraps mixed fields and groups', () => {
+      expect(
+        extractSettingsFields([stringField, groupFieldThreeDeep, stringField, groupFieldTwoDeep])
+      ).toEqual([stringField, nestedStringField, stringField, nestedStringField]);
+    });
+  });
+});
--- packages/public-api/src/devvit/internals/helpers/settingsUtils.ts
+++ packages/public-api/src/devvit/internals/helpers/settingsUtils.ts
@@ -2,8 +2,23 @@ import type { Metadata, ValidateFormRequest } from '@devvit/protos';
 import { ValidateFormResponse } from '@devvit/protos';
 import { makeAPIClients } from '../../../apis/makeAPIClients.js';
 import { getFormValues } from '../../../apis/ui/helpers/getFormValues.js';
-import type { OnValidateHandler, SettingsFormField } from '../../../types/settings.js';
+import type {
+  OnValidateHandler,
+  SettingsFormField,
+  SettingsFormFieldGroup,
+} from '../../../types/settings.js';
 import { getContextFromMetadata } from '../context.js';
+
+type SettingsPlainField = Exclude<SettingsFormField, SettingsFormFieldGroup>;
+
+export function extractSettingsFields(settings: SettingsFormField[]): SettingsPlainField[] {
+  return settings.flatMap((field) => {
+    if (field.type === 'group') {
+      return extractSettingsFields(field.fields);
+    }
+    return field;
+  }) as SettingsPlainField[];
+}
 
 export async function onValidateFormHelper(
   req: ValidateFormRequest,
@@ -20,20 +35,14 @@ export async function onValidateFormHelper(
   });
 
   const formValues = getFormValues(req.fieldValues);
-
-  const flattendFields = settings.flatMap((field) => {
-    if (field.type === 'group') {
-      return field.fields;
-    }
-    return field;
-  }) as SettingsFormField[];
+  const flattendFields = extractSettingsFields(settings);
 
   await Promise.all(
     flattendFields.map(async (field) => {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const fieldName = (field as any).name as string | undefined;
 
-      if (fieldName && field.type !== 'group' && field.onValidate) {
+      if (fieldName && field.onValidate) {
         const value = formValues[fieldName];
         const validator = field.onValidate as OnValidateHandler<unknown>;
 
