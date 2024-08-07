184e4c5e0c4b329261c94e6e4b40540f506ee39d
Redditbara <copybara@reddit.com>
2024-08-07 12:31:28 -0400

    Project import generated by Copybara.
    GitOrigin-RevId: 4e43c7fe47ee0206231937c67ef7b6d5daf28f98

---
+++ packages/cli/src/lib/hooks/init/check-node-version.ts
@@ -0,0 +1,30 @@
+import type { Hook } from '@oclif/core';
+import { createRequire } from 'node:module';
+
+const hook: Hook<'init'> = async function () {
+  // Verify that the user is using a supported version of Node.js, based on the engines property in package.json
+  const require = createRequire(import.meta.url);
+  const { engines } = require('../../../../package.json');
+
+  const currentVersion = process.version.substring(1); // remove the 'v' from the front
+  const requiredVersion = engines.node;
+  if (!requiredVersion.startsWith('>=')) {
+    this.error(
+      "Required version doesn't start with '>=', please check the package.json file and/or update the check-node-version hook."
+    );
+  }
+
+  const curVersion = currentVersion.split('.').map(Number);
+  const reqVersion = requiredVersion.substring(2).split('.').map(Number);
+  for (let i = 0; i < 3; i++) {
+    if (curVersion[i] < reqVersion[i]) {
+      this.error(
+        `Node.js ${requiredVersion} is required, but you are using Node.js ${currentVersion}. Check` +
+          `out our docs for notes on how to install the latest version of Node:\n` +
+          `https://developers.reddit.com/docs/get-started/environment#install-nodejs`
+      );
+    }
+  }
+};
+
+export default hook;
