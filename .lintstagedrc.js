01d8bb1312683820cc01af3d7c3b43791d366827
Redditbara <copybara@reddit.com>
2024-08-07 09:38:38 -0400

    Project import generated by Copybara.
    GitOrigin-RevId: 9209d80da48e82fbcb1f7dfb1a7c128a75b16480

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,13 +1,22 @@
 const path = require('path');
 const eslintConfigDir = path.join(__dirname, './packages/eslint-config/');
 
+// Since this passes every file along to the linters, and redlint ignores some
+// of our files, it'll throw a warning and refuse to let us commit. Thus, we
+// need to manually ignore the files here too.
+const JS_LINT_REGEX =
+  '!(' +
+    '(.lintstagedrc)|' +
+    '(.yarn/**/*)|' +
+    '(packages/cli/src/templates/pen/src/main)|' +
+    '(packages/eslint-config/base)|' +
+    '(__e2e__/playwright/**/*)|' +
+    '(packages/protos/src/types/**/*)' +
+  ').{cjs,js,jsx,mjs,cts,mts,ts,tsx}';
+
 module.exports = {
-  // This will match unwanted files like .lintstagedrc.js'*.{cjs,js,jsx,mjs,cts,mts,ts,tsx}'
-  '!((.lintstagedrc)|(.yarn/**/*)|(packages/cli/src/templates/pen/src/main)|packages/eslint-config/base|(__e2e__/playwright/**/*)).{cjs,js,jsx,mjs,cts,mts,ts,tsx}':
+  [JS_LINT_REGEX]:
     [
-      // Since this passes every file along to the linters, and redlint ignores some
-      // of our files, it'll throw a warning and refuse to let us commit. Thus, we
-      // need to manually ignore the files here too.
       `npx yarn exec redlint --fix --resolve-plugins-relative-to ${eslintConfigDir}`,
       'prettier --write',
     ],
