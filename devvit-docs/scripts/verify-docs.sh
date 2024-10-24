0c4d57b327faf4e8e901d8bfec7d8f1b4d25821b
Redditbara <copybara@reddit.com>
2024-10-24 09:59:55 -0400

    Project import generated by Copybara.
    GitOrigin-RevId: cec6cc1ed4e2762099fa61a4f813faf826411225

--- devvit-docs/scripts/verify-docs.sh
+++ devvit-docs/scripts/verify-docs.sh
@@ -6,8 +6,10 @@ G='\033[0;32m'   #'0;32' is Green's ANSI color code
 RESET='\033[0m'
 
 # Run generation script
-echo "Generating docs...";
-yarn gen:all
+echo "Generating API docs...";
+yarn gen:api-docs && yarn gen:reddit-api-docs
+echo "Linting API docs...";
+npx prettier --write '**/*.md'
 
 # Check for unstaged changes
 if [ -n "$(git status --porcelain)" ]; then
