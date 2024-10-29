0afdddf71c999ba4b558b2a270c207fb03d1fa8a
Redditbara <copybara@reddit.com>
2024-10-29 18:07:35 +0000

    Project import generated by Copybara.
    GitOrigin-RevId: de8180e112aadf12a78b351dc0871c80d84f398f

---
+++ packages/cli/src/templates/webviews/src/createPost.tsx
@@ -0,0 +1,26 @@
+import { Devvit } from '@devvit/public-api';
+
+Devvit.configure({
+  redditAPI: true,
+});
+
+Devvit.addMenuItem({
+  label: 'Create Web View Example Post',
+  location: 'subreddit',
+  onPress: async (_event, context) => {
+    const { reddit, ui } = context;
+    const subreddit = await reddit.getCurrentSubreddit();
+    const post = await reddit.submitPost({
+      title: 'Web View Example',
+      subredditName: subreddit.name,
+      // The preview appears while the post loads
+      preview: (
+        <vstack height="100%" width="100%" alignment="middle center">
+          <text size="large">Loading ...</text>
+        </vstack>
+      ),
+    });
+    ui.showToast({ text: 'Created post!' });
+    ui.navigateTo(post);
+  },
+});
