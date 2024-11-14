271c3db3e80f729017ba7014b697229e0246d92c
Redditbara <copybara@reddit.com>
2024-11-14 20:14:28 +0000

    Project import generated by Copybara.
    GitOrigin-RevId: 4a9d6f7684da0276ad5af4f026f227b12693b258

--- packages/cli/src/templates/web-views/src/main.tsx
+++ packages/cli/src/templates/web-views/src/main.tsx
@@ -1,34 +1,111 @@
 import './createPost.js';
 
-import { Devvit, type JSONValue } from '@devvit/public-api';
+import { Devvit, useAsync, useState } from '@devvit/public-api';
 
+// Defines the messages that are exchanged between Devvit and Web View
+type WebViewMessage = {
+  type: 'initialData' | 'setCounter' | 'updateCounter';
+  data: {
+    username?: string;
+    currentCounter?: number;
+    newCounter?: number;
+  };
+};
+
+// Configure Devvit's plugins
+Devvit.configure({
+  redditAPI: true,
+  redis: true,
+});
+
+// Add a custom post type to Devvit
 Devvit.addCustomPostType({
   name: 'Webview Example',
   height: 'tall',
   render: (context) => {
-    const onMessage = (msg: JSONValue): void => {
-      if (msg === 'ping') {
-        context.ui.webView.postMessage('myWebView', 'pong');
+    // Load username with `useAsync` hook
+    const { data: username } = useAsync(async () => {
+      const currUser = await context.reddit.getCurrentUser();
+      return currUser?.username ?? 'anon';
+    });
+
+    // Load latest counter from redis with `useAsync` hook
+    const { data: counter } = useAsync(async () => {
+      const redisCount = await context.redis.get(`counter_${context.postId}`);
+      console.log('redisCount:', redisCount);
+      return Number(redisCount ?? 0);
+    });
+
+    // Create a reactive state for web view visibility
+    const [webviewVisible, setWebviewVisible] = useState(false);
+
+    // When the web view invokes `window.parent.postMessage` this function is called
+    const onMessage = async (msg: WebViewMessage) => {
+      if (msg?.type === 'setCounter') {
+        const newCounter = msg.data.newCounter!;
+        await context.redis.set(`counter_${context.postId}`, newCounter.toString());
+        context.ui.webView.postMessage('myWebView', {
+          type: 'updateCounter',
+          data: {
+            currentCounter: newCounter,
+          },
+        });
       }
     };
 
+    // When the button is clicked, send initial data to web view and show it
+    const onShowWebviewClick = () => {
+      setWebviewVisible(true);
+      context.ui.webView.postMessage('myWebView', {
+        type: 'initialData',
+        data: {
+          username: username,
+          currentCounter: counter,
+        },
+      });
+    };
+
+    // Render the custom post type
     return (
       <vstack grow padding="small">
-        <text>WebView Content:</text>
-        <vstack border="thick" borderColor="black" grow>
-          <webview id="myWebView" url="page.html" onMessage={onMessage} grow />
+        <vstack
+          grow={!webviewVisible}
+          height={webviewVisible ? '0%' : '100%'}
+          alignment="middle center"
+        >
+          <text size="xlarge" weight="bold">
+            Example App
+          </text>
+          <spacer />
+          <vstack alignment="start middle">
+            <hstack>
+              <text size="medium">Username:</text>
+              <text size="medium" weight="bold">
+                {' '}
+                {username}
+              </text>
+            </hstack>
+            <hstack>
+              <text size="medium">Current counter:</text>
+              <text size="medium" weight="bold">
+                {' '}
+                {counter}
+              </text>
+            </hstack>
+          </vstack>
+          <spacer />
+          <button onPress={onShowWebviewClick}>Launch App</button>
         </vstack>
-        <vstack grow padding="small">
-          <text size="small" alignment="middle center">
-            Your webview app is working.{' '}
-          </text>
-          <text size="small" alignment="middle center">
-            If you can't see the content of page.html, you likely need to get your app allow-listed
-            for the webview feature.
-          </text>
-          <text size="small" alignment="middle center">
-            Get in touch with Reddit admins via Discord to allow-list your app.{' '}
-          </text>
+        <vstack grow={webviewVisible} height={webviewVisible ? '100%' : '0%'}>
+          <vstack border="thick" borderColor="black" height={webviewVisible ? '100%' : '0%'}>
+            <webview
+              id="myWebView"
+              url="page.html"
+              onMessage={(msg) => onMessage(msg as WebViewMessage)}
+              grow
+              height={webviewVisible ? '100%' : '0%'}
+            />
+          </vstack>
         </vstack>
       </vstack>
     );
--- packages/cli/src/templates/web-views/webroot/script.js
+++ packages/cli/src/templates/web-views/webroot/script.js
@@ -4,18 +4,53 @@
 class App {
   constructor() {
     const output = document.querySelector('#messageOutput');
+    const increaseButton = document.querySelector('#btn-increase');
+    const decreaseButton = document.querySelector('#btn-decrease');
+    const usernameLabel = document.querySelector('#username');
+    const counterLabel = document.querySelector('#counter');
+    var counter = 0;
+
+    // When the Devvit app sends a message with `context.ui.webView.postMessage`, this will be triggered
     window.addEventListener('message', (ev) => {
       const { type, data } = ev.data;
 
+      // Reserved type for messages sent via `context.ui.webView.postMessage`
       if (type === 'devvit-message') {
         const { message } = data;
+
+        // Always output full message
         output.replaceChildren(JSON.stringify(message, undefined, 2));
+
+        // Load initial data
+        if (message.type === 'initialData') {
+          const { username, currentCounter } = message.data;
+          console.log('Initial data:', usernameLabel, username, counterLabel, currentCounter);
+          usernameLabel.innerText = username;
+          counterLabel.innerText = counter = currentCounter;
+        }
+
+        // Update counter
+        if (message.type === 'updateCounter') {
+          const { currentCounter } = message.data;
+          counterLabel.innerText = counter = currentCounter;
+        }
       }
     });
 
-    const pingButton = document.querySelector('button');
-    pingButton.addEventListener('click', () => {
-      window.parent?.postMessage({ type: 'ping' }, '*');
+    increaseButton.addEventListener('click', () => {
+      // Sends a message to the Devvit app
+      window.parent?.postMessage(
+        { type: 'setCounter', data: { newCounter: Number(counter + 1) } },
+        '*'
+      );
+    });
+
+    decreaseButton.addEventListener('click', () => {
+      // Sends a message to the Devvit app
+      window.parent?.postMessage(
+        { type: 'setCounter', data: { newCounter: Number(counter - 1) } },
+        '*'
+      );
     });
   }
 }
