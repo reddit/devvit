ac917b003a59a94ec63b85d49022b224c6a13e6b
Nisarga <git@nisarga.me>
2025-01-16 21:37:03 +0530

    Update main.ts
    Signed-off-by: Nisarga <git@nisarga.me>

--- packages/apps/comment-nuke/src/main.ts
+++ packages/apps/comment-nuke/src/main.ts
@@ -1,11 +1,11 @@
 import { Devvit } from '@devvit/public-api';
-import { handleNuke } from './nuke.js';
-
+import { handleNuke, handleNukePost } from './nuke.js'; 
+ 
 Devvit.configure({
   redditAPI: true,
   modLog: true,
 });
-
+ 
 const nukeForm = Devvit.createForm(
   () => {
     return {
@@ -34,6 +34,7 @@ const nukeForm = Devvit.createForm(
       cancelLabel: 'Cancel',
     };
   },
+ 
   async ({ values }, context) => {
     if (context.commentId) {
       const result = await handleNuke(
@@ -53,7 +54,7 @@ const nukeForm = Devvit.createForm(
     }
   }
 );
-
+ 
 Devvit.addMenuItem({
   label: 'Mop comments',
   description: 'Remove this comment and all child comments. This might take a few seconds to run.',
@@ -63,5 +64,66 @@ Devvit.addMenuItem({
     context.ui.showForm(nukeForm);
   },
 });
-
+ 
+// Another form for nuking all comments of a post
+ 
+const nukePostForm = Devvit.createForm(
+  () => {
+    return {
+      fields: [
+        {
+          name: 'remove',
+          label: 'Remove comments',
+          type: 'boolean',
+          defaultValue: true,
+        },
+        {
+          name: 'lock',
+          label: 'Lock comments',
+          type: 'boolean',
+          defaultValue: false,
+        },
+        {
+          name: 'skipDistinguished',
+          label: 'Skip distinguished comments',
+          type: 'boolean',
+          defaultValue: false,
+        },
+      ],
+      title: 'Mop Post Comments',
+      acceptLabel: 'Mop',
+      cancelLabel: 'Cancel',
+    };
+  },
+  async ({ values }, context) => {
+    if (context.postId) {
+      const result = await handleNukePost(
+        {
+          remove: values.remove,
+          lock: values.lock,
+          skipDistinguished: values.skipDistinguished,
+          postId: context.postId,
+          subredditId: context.subredditId,
+        },
+        context
+      );
+      console.log(`Mop result - ${result.success ? 'success' : 'fail'} - ${result.message}`);
+      context.ui.showToast(`${result.success ? 'Success' : 'Failed'} : ${result.message}`);
+    } else {
+      context.ui.showToast(`Mop failed! Please try again later.`);
+    }
+  }
+);
+ 
+Devvit.addMenuItem({
+  label: 'Mop post comments',
+  description: 'Remove all comments of this post. This might take a few seconds to run.',
+  location: 'post',
+  forUserType: 'moderator',
+  onPress: (_, context) => {
+    context.ui.showForm(nukePostForm);
+  },
+});
+ 
+ 
 export default Devvit;
