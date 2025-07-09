f638127258f79d040e8795ae72d44b0467ce5a80
Redditbara <copybara@reddit.com>
2025-07-09 14:44:13 -0400

    Project import generated by Copybara.
    GitOrigin-RevId: 53d33c04a64ce34edd3c9f1011202a02df7d793e

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,4 +1,6 @@
 export * from './types/form.js';
+export * from './types/menu-item.js';
+export * from './types/triggers.js';
 export * from './types/ui-response.js';
 export * from '@devvit/shared-types/json.js';
 export * from '@devvit/shared-types/tid.js';
