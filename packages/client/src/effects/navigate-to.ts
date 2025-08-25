e6ac1a7cd3cfb5eafcc7408014b8379323a243bc
Redditbara <copybara@reddit.com>
2025-08-25 17:07:20 -0400

    Project import generated by Copybara.
    GitOrigin-RevId: 0ee3199e76390d3958f31f4afe1f28c5a3420afe

--- packages/client/src/effects/navigate-to.ts
+++ packages/client/src/effects/navigate-to.ts
@@ -1,5 +1,4 @@
 import type { EffectType } from '@devvit/protos/types/devvit/ui/effects/v1alpha/effect.js';
-import type { Comment, Post, Subreddit, User } from '@devvit/reddit';
 import { emitEffect } from '@devvit/shared-types/client/emit-effect.js';
 
 /**
@@ -7,14 +6,7 @@ import { emitEffect } from '@devvit/shared-types/client/emit-effect.js';
  *
  * @param thingOrUrl - The URL, subreddit, post, comment, or user to navigate to
  */
-export function navigateTo(url: string): void;
-export function navigateTo(subreddit: Readonly<Subreddit>): void;
-export function navigateTo(post: Readonly<Post>): void;
-export function navigateTo(comment: Readonly<Comment>): void;
-export function navigateTo(user: Readonly<User>): void;
-export function navigateTo(
-  thingOrUrl: string | Readonly<Subreddit> | Readonly<Post> | Readonly<Comment> | Readonly<User>
-): void {
+export function navigateTo(thingOrUrl: string | { readonly url: string }): void {
   const inputUrl = typeof thingOrUrl === 'string' ? thingOrUrl : thingOrUrl.url;
   if (!URL.canParse(inputUrl)) {
     throw new TypeError(`Invalid URL: ${inputUrl}`);
