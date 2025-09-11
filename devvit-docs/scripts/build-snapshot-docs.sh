744c38dada8134680458b6504f455fc5e5cffa51
Redditbara <copybara@reddit.com>
2025-09-11 13:34:00 -0700

    Project import generated by Copybara.
    GitOrigin-RevId: 0ad852091ed38ad310efed64f9d0c601a7e0018b

--- devvit-docs/scripts/build-snapshot-docs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-set -${V:+x}eu
-
-scriptDir="$(realpath "$(dirname "$0")")"
-
-cd $scriptDir
-node writeSnapshotSchemas.js
-echo "Generate Snapshot schema docs from schemas in dw-airflow"
