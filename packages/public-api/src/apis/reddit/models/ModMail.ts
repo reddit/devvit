90c0fbc91b63f497143629e98fcc507c0f44aec7
BinaryANT <96654436+DNSCond@users.noreply.github.com>
2025-10-29 22:23:45 +0100

    fixed-inconsistency-conversationType (#191)
    https://discord.com/channels/1050224141732687912/1050227353311248404/1390110420349620305
    
    ## 💸 TL;DR
    <!-- What's the three sentence summary of purpose of the PR -->
    i fixed the incorrect documentation of
    https://developers.reddit.com/docs/api/redditapi/models/type-aliases/ConversationData#conversationtype
    by swapping `internal` and `sr_user`. yes that is literally all i did.
    
    ## ✅ Checks
    <!-- Make sure your pr passes the CI checks and do check the following
    fields as needed - -->
    - [ ] CI tests (if present) are passing
    - [ ] Adheres to code style for repo
    - [x] Contributor License Agreement (CLA) completed if not a Reddit
    employee

--- packages/public-api/src/apis/reddit/models/ModMail.ts
+++ packages/public-api/src/apis/reddit/models/ModMail.ts
@@ -191,8 +191,8 @@ export type ConversationData = {
   /**
    * A ConversationType specifies whether a conversation is with a subreddit
    * itself, with another user, or with another subreddit entirely.
-   * - `internal` - This is a conversation with another user outside of the subreddit. The participant ID is that user's ID.
-   * - `sr_user` - This is a Mod Discussion, internal to the subreddit. There is no other participant.
+   * - `sr_user` - This is a conversation with another user outside of the subreddit. The participant ID is that user's ID.
+   * - `internal` - This is a Mod Discussion, internal to the subreddit. There is no other participant.
    * - `sr_sr` - This is a conversation is with another subreddit. The participant will have a subreddit ID.
    */
   conversationType?: string;
